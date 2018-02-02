6cc2b438c7e23c352c6f450c513d15dc7de5e148
Charles Howard <frodare@gmail.com>
2018-02-02 07:39:47 -0500

    rift keystone is now consumed on use

--- src/main/java/net/torocraft/rifts/items/ItemRiftKeyStone.java
+++ src/main/java/net/torocraft/rifts/items/ItemRiftKeyStone.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -55,6 +56,7 @@ public class ItemRiftKeyStone extends Item {
       return EnumActionResult.PASS;
     }
     if (PortalUtil.openRiftPortal(player, pos.up(), facing)) {
+      player.setHeldItem(hand, ItemStack.EMPTY);
       return EnumActionResult.SUCCESS;
     }
     return EnumActionResult.FAIL;
